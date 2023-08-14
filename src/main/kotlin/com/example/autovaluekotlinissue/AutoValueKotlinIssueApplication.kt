58f6ce1be82931041f956915bdf4a93197fccc6a
Nils Breunese <nils@breun.nl>
2023-08-14 14:07:10 +0200

    Initial commit

---
+++ src/main/kotlin/com/example/autovaluekotlinissue/AutoValueKotlinIssueApplication.kt
@@ -0,0 +1,11 @@
+package com.example.autovaluekotlinissue
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AutoValueKotlinIssueApplication
+
+fun main(args: Array<String>) {
+	runApplication<AutoValueKotlinIssueApplication>(*args)
+}
